1eb9d7a0b04f1279c86e0262428d11e282d20c77
tiennguyen <tiennguyencc2013@email.com>
2020-09-08 20:51:32 +0700

    cvnvbn

--- app/src/main/java/com/softtphongkham/random_25082020/MainActivity.java
+++ app/src/main/java/com/softtphongkham/random_25082020/MainActivity.java
@@ -3,6 +3,7 @@ package com.softtphongkham.random_25082020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        float number = 1.5f;
-        
+       // float number = 1.5f;
+        //int result = Math.round(number);
+        //Log.d(tag: "BBB", String.valueOf(result));
+        double result = Math.random();
+        Log.d("BBB", String.valueOf(result));
     }
 }
